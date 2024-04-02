a7afbfea561a58d5cf6269fa30b64216c2e59a4a
Adrian Valente <adrian.valente@ens.fr>
2024-04-02 17:23:11 +0200

    correct env

--- src/annotate_wikipedia.py
+++ src/annotate_wikipedia.py
@@ -4,23 +4,21 @@ from annotator import Annotator
 from loaders import load_wikipedia
 
 if __name__ == '__main__':
-    argparser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
     # Positional arguments
-    argparser.add_argument(
+    parser.add_argument(
         'query', 
         type=str, 
-        help='The query to search for in Wikipedia', 
-        required=True
+        help='The query to search for in Wikipedia'
     )
-    argparse.add_argument(
+    parser.add_argument(
         'db',
         type=str,
-        help='The database location',
-        required=True
+        help='The database location'
     )
-    argparse.add_argument('-v', 'verbose', action='store_true', help='Verbose mode')
-    args = argparser.parse_args()
+    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode')
+    args = parser.parse_args()
     
     doc = load_wikipedia(args.query)
-    annotator = Annotator(db_location=args.db, verbose=int(args.verbose))
+    annotator = Annotator(db_location=args.db, verbosity=int(args.verbose))
     annotator.annotate(doc)
